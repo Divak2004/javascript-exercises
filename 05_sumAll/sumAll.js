06b4602faca9ffe97ea9dfa332dfadaa48fcd0df
Divak2004 <fu_david@outlook.com>
2024-07-23 16:09:12 +0800

    Completed ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0
+    if (typeof(start) != "number" || typeof(end) != "number" || start < 0 ||
+    end < 0) {
+        return "ERROR"
+    }
+    for (let i = Math.min(start, end); i < Math.max(start+1, end+1); i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
