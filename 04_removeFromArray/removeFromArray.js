428c18f59b994e66a4edaee5833f3f519f4aa40d
Divak2004 <fu_david@outlook.com>
2024-07-23 16:00:35 +0800

    Completed ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    let res = []
+    arr.forEach(element => {
+        if (!(nums.includes(element))) {
+            res.push(element)
+        }
+    });
+    return res
 };
 
 // Do not edit below this line
