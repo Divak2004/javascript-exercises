2dbf0f45916ad19c48b0ceb25a61ae25d759d0ee
Divak2004 <fu_david@outlook.com>
2024-07-23 15:45:27 +0800

    Completed ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    res = ""
+    for (let i = str.length-1; i > -1; i--) {
+        res += str[i]
+    }
+    return res
 };
 
 // Do not edit below this line
